59248dbb6320cd5cc636707419774b3ce7221376
Yizhoulu <luyizhou@umich.edu>
2019-07-19 13:03:34 -0400

    Update Virtual Constraint to fit (sw,ext) configuration

--- frost-dev-master/example/minitaur/+sys/AddVirtualConstraint.m
+++ frost-dev-master/example/minitaur/+sys/AddVirtualConstraint.m
@@ -4,14 +4,10 @@ p = SymVariable('p',[2,1]);
 tau = (t-p(1))/(p(2)-p(1));
 q = domain.States.x;
 ya_2 = [...
-    (q('motor_front_leftL_joint') - q('motor_front_leftR_joint'))/2+pi;
-     q('motor_front_leftL_joint') + q('motor_front_leftR_joint');
-    (q('motor_back_leftL_joint')  - q('motor_back_leftR_joint'))/2+pi;
-     q('motor_back_leftL_joint')  + q('motor_back_leftR_joint');
-   (-q('motor_front_rightL_joint')+ q('motor_front_rightR_joint'))/2+pi;
-     q('motor_front_rightL_joint')+ q('motor_front_rightR_joint');
-   (-q('motor_back_rightL_joint') + q('motor_back_rightR_joint'))/2+pi;
-     q('motor_back_rightL_joint') + q('motor_back_rightR_joint')];
+    (q(8) - q(7))/2; q(7) + q(8);
+    (q(12)- q(11))/2;q(11)+ q(12);
+    (q(15)- q(16))/2;q(16)+ q(15);
+    (q(19)- q(20))/2;q(20)+ q(19)    ];
 y2_label = {'front_left_angle','front_left_length','back_left_angle','back_left_length','front_right_angle','front_right_length','back_right_angle','back_right_length'};
 y2 = VirtualConstraint(domain,ya_2,'output','DesiredType','Bezier','PolyDegree',5,...
         'RelativeDegree',2,'OutputLabel',{y2_label},'PhaseType','TimeBased',...
