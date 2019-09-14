73023ccc68b70e90e5908fa5f2b74fc6434914bc
Yizhoulu <luyizhou@umich.edu>
2019-09-14 00:21:18 -0400

    verified and finished gaitlibrary_pybullet2

--- frost-dev-master/example/minitaur/GaitLibrary/FindSolution_Bezier_PyBullet2.m
+++ frost-dev-master/example/minitaur/GaitLibrary/FindSolution_Bezier_PyBullet2.m
@@ -1,26 +1,27 @@
 function [gaitparams, DesiredLegSwing_Relative] = FindSolution_Bezier_PyBullet2(current_velocity, current_phase, phasing)
 if nargin < 1
     % just for example purpose
-    current_velocity = 0.57;
+    current_velocity = 0.49;
     current_phase = 1;
-    phasing = 0.32;
+    phasing = 0.2;
 end
 load('GaitLibrary_PyBullet2.mat','GaitLibrary')
 % Control Policy locates the correct phase the robot is in the GaitLibrary
 gaitparams = ControlPolicy(current_velocity, GaitLibrary, current_phase);
 % current_velocity
-LegPose = reshape(gaitparams.LegPose(1,:,:),3,21);
-DesiredLegSwing_Relative = interp1(linspace(0,1,21), LegPose(1,:), phasing);
-DesiredLegSwing_Absolute = interp1(linspace(0,1,21), LegPose(2,:), phasing);
-DesiredLegExtension = interp1(linspace(0,1,21), LegPose(3,:), phasing);
+LegPose = reshape(gaitparams.LegPose(1,:,:), 3, 21);
+Time = reshape(gaitparams.Time, 1, 21);
+DesiredLegSwing_Relative = interp1(Time, LegPose(1,:), 0.24);
+DesiredLegSwing_Absolute = interp1(Time, LegPose(2,:), 0.24);
+DesiredLegExtension = interp1(Time, LegPose(3,:), 0.24);
 
 figure(1); hold on; grid on; 
-plot(linspace(0,1,21), LegPose(1,:), 'b', 'linewidth', 2)
-plot(linspace(0,1,21), LegPose(2,:), 'r', 'linewidth', 2)
-plot(linspace(0,1,21), LegPose(3,:), 'y', 'linewidth', 2)
-scatter(phasing, DesiredLegSwing_Relative, 'g', 'linewidth', 2);
-scatter(phasing, DesiredLegSwing_Absolute, 'g', 'linewidth', 2);
-scatter(phasing, DesiredLegExtension, 'g', 'linewidth', 2);
+plot(Time, LegPose(1,:), 'b', 'linewidth', 2)
+plot(Time, LegPose(2,:), 'r', 'linewidth', 2)
+plot(Time, LegPose(3,:), 'y', 'linewidth', 2)
+scatter(0.24, DesiredLegSwing_Relative, 'g', 'linewidth', 2);
+scatter(0.24, DesiredLegSwing_Absolute, 'g', 'linewidth', 2);
+scatter(0.24, DesiredLegExtension, 'g', 'linewidth', 2);
 end
 
 function gaitparams = ControlPolicy(phi, GaitLibrary, current_phase)
@@ -28,8 +29,10 @@ phi = clamp(phi, GaitLibrary.Velocity(1,1), GaitLibrary.Velocity(1,end));
 
 if current_phase == 1
     gaitparams.LegPose = interp1(GaitLibrary.Velocity(1,:), GaitLibrary.FrontStance.LegPose, phi);
+    gaitparams.Time = interp1(GaitLibrary.Velocity(1,:), GaitLibrary.FrontStance.Time, phi);
 elseif current_phase == 5
     gaitparams.LegPose = interp1(GaitLibrary.Velocity(1,:), GaitLibrary.BackStance.LegPose, phi);
+    gaitparams.Time = interp1(GaitLibrary.Velocity(1,:), GaitLibrary.FrontStance.Time, phi);
 else
     disp('Unrecognized Phase')
 end
